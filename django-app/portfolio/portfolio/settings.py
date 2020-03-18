269b629ed924678ee0a0f792a9292c38e806fc6a
AdityaVandan <van13heisenberg@gmail.com>
2020-03-18 19:45:08 +0530

    just checking

--- django-app/portfolio/portfolio/settings.py
+++ django-app/portfolio/portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resume.apps.ResumeConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'portfoliodb',
+        'USER':'aditya',
+        'PASSWORD':'aditya',
+        'HOST':'localhost',
+        'PORT':'3306',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- django-app/portfolio/resume/models.py
+++ django-app/portfolio/resume/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class school(models.Model):
+    name=models.CharField(max_length=200)
+    degree=models.CharField(max_length=200)
+    start=models.CharField(max_length=100)
+    
