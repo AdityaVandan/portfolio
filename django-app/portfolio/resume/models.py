49cae6e46e6145017d18a99eeb70d42d6459516d
AdityaVandan <van13heisenberg@gmail.com>
2020-03-18 20:29:19 +0530

    models created

--- django-app/portfolio/resume/models.py
+++ django-app/portfolio/resume/models.py
@@ -1,8 +1,50 @@
 from django.db import models
 
 # Create your models here.
-class school(models.Model):
+class School(models.Model):
     name=models.CharField(max_length=200)
-    degree=models.CharField(max_length=200)
-    start=models.CharField(max_length=100)
-    
+    degree=models.CharField(max_length=100)
+    start=models.DateField()
+    end=models.DateField()
+    marks_type=models.CharField()
+    marks=models.CharField()
+
+class Skill(models.Model):
+    name=models.CharField(max_length=100)
+    image_path=models.CharField(max_length=100)
+
+class Interest(models.Model):
+    name=models.CharField(max_length=100)
+
+class Achievement(models.Model):
+    name=models.CharField(max_length=100)
+
+class Experience(models.Model):
+    post=models.CharField(max_length=100)
+    company=models.CharField(max_length=100)
+    description=models.CharField(max_length=1000)
+    start=models.DateField()
+    end=models.DateField()
+
+class KeyPoint(models.Model):
+    experience_id=models.ForeignKey(Experience,on_delete=models.CASCADE)
+    point=models.CharField(max_length=300)
+
+class Project(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=3000)
+    repositoryLink=models.CharField(max_length=2048)
+
+class Technology(models.Model):
+    name=models.CharField(max_length=100)
+    imagePath=model.CharField(max_length=2048)
+
+class ScreenShot(models.Model):
+    project_id=models.ForeignKey(Project,on_delete=models.CASCADE)
+    path=models.CharField(max_length=2048)
+    name=models.CharField(max_length=100)
+
+class Video(models.Model):
+    project_id=models.ForeignKey(Project,on_delete=models.CASCADE)
+    path=models.CharField(max_length=2048)
+    name=models.CharField(max_length=100)
